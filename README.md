e0e4b088d2a26a880ce4c41ecc9a42ef9fb6dee5
Yoshiori Shoji <yoshiori@gmail.com>
2025-02-14 09:33:43 +0900

    Dead end

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # apple-music
 =======
 
+The page was displayed, but the music didn’t play, so this project is over.
+
+
+![screenshot](screenshot.png)
+
 This template should help get you started developing with Tauri in vanilla HTML, CSS and Typescript.
-
-## Recommended IDE Setup
-
-- [VS Code](https://code.visualstudio.com/) + [Tauri](https://marketplace.visualstudio.com/items?itemName=tauri-apps.tauri-vscode) + [rust-analyzer](https://marketplace.visualstudio.com/items?itemName=rust-lang.rust-analyzer)
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,15 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::Manager;
+
 fn main() {
-    apple_music_lib::run()
+    tauri::Builder::default()
+    .setup(|app| {
+        let window = app.get_webview_window("main").unwrap();
+        let _ = window.eval("window.location.replace('https://beta.music.apple.com/')");
+        Ok(())
+      })
+      .run(tauri::generate_context!())
+      .expect("error running tauri app");
 }
